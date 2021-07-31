7e94af65c21052365c4be00ee10b3f96f75b3695
Andrey Cesar <andreycesarrodrigues@hotmail.com>
2021-07-31 15:35:31 -0300

    Criando o componente Card

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, TextInput, Button} from 'react-native';
+
+import Card from '../components/Card';
 
 const StartGameScreen = props =>{
     return(
         <View style={styles.screen}>
-            <Text>A tela do jogo!</Text>
+            <Text style={styles.title}>Comece um jogo novo!</Text>
+            <Card style={styles.inputContainer}>
+                <Text>Selecione um numero</Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <Button title="Começar denovo" onPress={()=>{}}/>
+                    <Button title="Confirmar"/>
+                </View>
+            </Card>
         </View>
     );
 };
@@ -14,8 +24,24 @@ const styles =StyleSheet.create({
         flex:1,
         padding: 10,
         alignItems: 'center',
-        justifyContent: 'flex-start',
-    }
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 400,
+        maxWidth: '80%',
+        alignItems: 'center',
+        padding: 20,
+        borderRadius: 10,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+    },
 });
 
 export default StartGameScreen;
